46668bfd7c133677d6bc881c8dd7e7444b92cdc4
ankush1026 <ankushdogra111@gmail.com>
2023-06-17 14:00:45 +0530

    Basic Node

---
+++ importExport/index.js
@@ -0,0 +1,5 @@
+const {add,sub,fname} = require('./oper');
+
+console.log(add(4,5));
+console.log(sub(6,3));
+console.log(fname);
---
+++ importExport/oper.js
@@ -0,0 +1,11 @@
+const add = (a,b) => {
+    return a+b;
+}
+
+const sub = (a,b) => {
+    return a-b;
+}
+
+const fname='Ankush';
+
+module.exports = {add,sub,fname};
